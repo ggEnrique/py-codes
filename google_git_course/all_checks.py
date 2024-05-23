2519a0202ab8fe7f54210a943816a4f4871350a0
Enrique Garcia <gg.enrique2015@gmail.com>
2024-05-22 22:06:33 -0600

    fixed exist to exists

--- google_git_course/all_checks.py
+++ google_git_course/all_checks.py
@@ -3,7 +3,7 @@ import os
 
 def check_reboot():
     """Returns True if the computer has a pending reboot"""
-    return os.path.exist("/run/reboot-required")
+    return os.path.exists("/run/reboot-required")
     
 def main():
     pass
